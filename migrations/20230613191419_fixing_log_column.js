69e297484710672a8e15c6a1ba71c3f8f6945c4c
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-06-13 15:26:54 -0400

    changed amount from integer to float

---
+++ migrations/20230613191419_fixing_log_column.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.alterTable('logs', function (table) {
+        table.float('amount').notNullable().alter();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.alterTable('logs', function (table) {
+        table.integer('amount').notNullable().alter();
+    });
+};
